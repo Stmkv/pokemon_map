fb85d7c11046c84435b6101c240a0de2e8e2ed71
Stmkv <stepkamakarov1999@mail.ru>
2024-08-11 01:40:40 +0300

    Оптитмизировал поля

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,8 +4,8 @@ from django.db import models  # noqa F401
 # your models here
 class Pokemon(models.Model):
     title_ru = models.CharField(max_length=200, verbose_name='Имя на русском языке')
-    title_en = models.CharField(max_length=200, verbose_name='Имя на английском')
-    title_jp = models.CharField(max_length=200, verbose_name='Имя на японском')
+    title_en = models.CharField(null=True, blank=True, max_length=200, verbose_name='Имя на английском')
+    title_jp = models.CharField(null=True, blank=True, max_length=200, verbose_name='Имя на японском')
     image = models.ImageField(null=True, blank=True, verbose_name="Изображение")
     description = models.TextField(null=True, blank=True, verbose_name="Описание")
     previous_evolution = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='next_evolution', verbose_name="Предыдущая эволюция")
@@ -16,10 +16,10 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name="pokemon_entities", verbose_name="Покемон")
-    lat = models.FloatField(null=True, blank=True, verbose_name='Широта')
-    lon = models.FloatField(null=True, blank=True, verbose_name='Долгота')
-    appeared_at = models.DateTimeField(null=True, blank=True, verbose_name='Появится в')
-    disappear_at = models.DateTimeField(null=True, blank=True, verbose_name='Убежит в')
+    lat = models.FloatField(verbose_name='Широта')
+    lon = models.FloatField(verbose_name='Долгота')
+    appeared_at = models.DateTimeField(verbose_name='Появится в')
+    disappear_at = models.DateTimeField(verbose_name='Убежит в')
     level = models.IntegerField(null=True, blank=True, verbose_name='Уровень')
     health = models.IntegerField(null=True, blank=True, verbose_name='Здоровье')
     strength = models.IntegerField(null=True, blank=True, verbose_name='Сила')
